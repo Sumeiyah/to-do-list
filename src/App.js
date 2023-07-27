b83f89a62dbe64f9e14e97c9319990cd77a1ed3d
sumeiyah <sumeyafarah171400>
2023-07-27 09:17:52 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TodoList = () => {
+    const [inputTask, setInputTask] = useState('');
+    const [list, setList] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-export default App;
+    const handleAddTodo = () => {
+        const newTask = {
+            id: Math.random(),
+            todo: inputTask
+        };
+
+       setList([...list, newTask]);
+        setInputTask('');
+    };
+
+   const handleDeleteTodo = (id) => {
+        const newList = list.filter((todo) => todo.id !== id);
+        setList(newList);
+    };
+
+   const handleInputChange = (event) => {
+        setInputTask(event.target.value);
+    };
+
+    useEffect(()=> {
+
+        let url = 'http://localhost:4000/posts'
+
+        fetch(url).then(res => res.json()).then(data => setPosts(data))
+    }, [])
+
+   return (
+        <div className="Todo">
+            <h1>My To-Do List</h1>
+        
+            {posts.map(post => <li>{post.author}</li>)}
+            <div className="Top">
+                <input className="input" type="text" value={inputTask}
+                   onChange={handleInputChange} placeholder="Enter a task" />
+
+                <button className="btn" onClick={handleAddTodo}>ADD</button>
+            </div>
+
+           <ul>
+                { list.map((todo) => (
+                    <li className="task" key={todo.id}>
+                        {todo.todo}
+                        <button onClick={() => handleDeleteTodo(todo.id)}>
+                           Delete
+                       </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList; 
+
+
